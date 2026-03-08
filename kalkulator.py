cf3e543f591d1ee3f57d85acef309f86d98d6fd8
William-Tanujaya <wetenugas@gmail.com>
2026-03-08 18:31:29 +0700

    Kelompok Ignas

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,19 @@
-#please add your calculator function here
+angka1 = float(input("Masukkan angka pertama: "))
+operator = input("Masukkan operator (+, -, *, /): ")
+angka2 = float(input("Masukkan angka kedua: "))
+
+if operator == "+":
+    hasil = angka1 + angka2
+elif operator == "-":
+    hasil = angka1 - angka2
+elif operator == "*":
+    hasil = angka1 * angka2
+elif operator == "/":
+    if angka2 != 0:
+        hasil = angka1 / angka2
+    else:
+        hasil = "Error: tidak bisa dibagi 0"
+else:
+    hasil = "Operator tidak valid"
+
+print("Hasil:", hasil)#please add your calculator function here
